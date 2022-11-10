bf66490b728d2a1446a7308611e65dc594562ee3
Martin Kirilov <wucdbm@gmail.com>
2022-11-10 10:20:41 +0200

    Remove transition to reduce confusion with similar issues

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,53 @@
 <template>
     <router-view v-slot="{ Component }">
-        <transition name="fade">
-            <div
-                v-if="!isInitialized"
-                key="loader"
-            >
-                L O A D I N G
-            </div>
+        <div
+            v-if="!isInitialized"
+            key="loader"
+        >
+            L O A D I N G
+        </div>
 
-            <div
-                v-else-if="initError"
-                key="init-error"
-            >
-                INIT ERROR
-                <br>
-                {{ initError }}
-            </div>
+        <div
+            v-else-if="initError"
+            key="init-error"
+        >
+            INIT ERROR
+            <br>
+            {{ initError }}
+        </div>
 
-            <div
-                v-else
-                key="content"
+        <div
+            v-else
+            key="content"
+        >
+            <router-link
+                class="button"
+                :to="{name: ROUTE_HOME}"
             >
-                <router-link
-                    class="button"
-                    :to="{name: ROUTE_HOME}"
-                >
-                    Home
-                </router-link>
-                <router-link
-                    class="button"
-                    :to="{name: ROUTE_CHARACTERS}"
-                >
-                    Characters
-                </router-link>
-                <br>
-                <h4>Found {{ episodes.length }} episodes</h4>
-                <h5>Suppose this ^ is our dynamic menu that must be preloaded</h5>
-                <br>
-                <img
-                    src="@/assets/vue.svg"
-                    alt="Vue logo"
-                >
-                <br>
-                <br>
-                <hr>
-                <br>
-                <div style="border: 1px solid red; padding: 20px;">
-                    <component :is="Component" />
-                </div>
+                Home
+            </router-link>
+            <router-link
+                class="button"
+                :to="{name: ROUTE_CHARACTERS}"
+            >
+                Characters
+            </router-link>
+            <br>
+            <h4>Found {{ episodes.length }} episodes</h4>
+            <h5>Suppose this ^ is our dynamic menu that must be preloaded</h5>
+            <br>
+            <img
+                src="@/assets/vue.svg"
+                alt="Vue logo"
+            >
+            <br>
+            <br>
+            <hr>
+            <br>
+            <div style="border: 1px solid red; padding: 20px;">
+                <component :is="Component" />
             </div>
-        </transition>
+        </div>
     </router-view>
 </template>
 
@@ -79,25 +77,3 @@ export default defineComponent({
     }
 })
 </script>
-
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.45s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-    opacity: 0;
-}
-
-.fade-route-enter-active,
-.fade-route-leave-active {
-    transition: opacity 0.12s ease;
-}
-
-.fade-route-enter-from,
-.fade-route-leave-to {
-    opacity: 0;
-}
-</style>
